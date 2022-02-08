c18f02c21e67052cd90d9f4565e97b68ef89331c
yeos <seanyeosr@gmail.com>
2022-02-08 19:30:53 +0800

    CreateNewUser integrated into login as modal

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -1,6 +1,7 @@
-import React, { useReducer } from "react";
-import { Button, TextField } from "@mui/material/";
+import React, { useReducer, useState } from "react";
+import { Button, TextField, Modal, Box } from "@mui/material/";
 import PropTypes from "prop-types";
+import CreateNewUser from "./CreateNewUser";
 const axios = require("axios");
 
 // get imput
@@ -9,6 +10,18 @@ const axios = require("axios");
 // success save token
 // show cards for all entries for user.
 // fail show error
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 const loginReducer = (loginState, action) => {
   switch (action.type) {
@@ -27,6 +40,9 @@ const loginReducer = (loginState, action) => {
 };
 
 const Login = ({ setToken, setEmail }) => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   const [loginState, loginDispatcher] = useReducer(loginReducer, {
     email: "",
     password: "",
@@ -75,7 +91,14 @@ const Login = ({ setToken, setEmail }) => {
       <Button variant="contained" onClick={() => handleSubmit(loginState)}>
         Log In
       </Button>
-      <Button variant="contained">Create New Account</Button>
+      <Button variant="contained" onClick={handleOpen}>
+        Create New Account
+      </Button>
+      <Modal open={open} onClose={handleClose}>
+        <Box sx={style}>
+          <CreateNewUser />
+        </Box>
+      </Modal>
     </>
   );
 };
